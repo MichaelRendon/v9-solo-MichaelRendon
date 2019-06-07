b84cc8c87b9083441b97d5b16847f64c27d40d4c
MichaelRendon <49463393+MichaelRendon@users.noreply.github.com>
2019-06-06 21:20:57 -0400

    initial react app upload with components commented-out

---
+++ portfolio/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+
+    <h1> This is my future portfolio.  Stay tuned for updates!</h1>
+{/*
+    <Navigation />
+    <Bio />
+    <Projects />
+    <Education />
+    <Experience />*/}
+      
+    </div>
+  );
+}
+
+export default App;
